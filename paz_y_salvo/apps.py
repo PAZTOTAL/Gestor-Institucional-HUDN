437d201d683b5bda6bd4faadc8e59cf10f361aaf
David Solarte <dsolarte2809@gmail.com>
2026-04-24 10:01:13 -0500

    Revert "Merge pull request #17 from PAZTOTAL/pazysalvo"
    This reverts commit cc351bdf0b91b37a32ff26ebf9fcc1b6fa956cce, reversing
    changes made to cf48393dd0164b484474a0fa3616fccbeda66e6a.

--- paz_y_salvo/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PazYSalvoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'paz_y_salvo'
-    verbose_name = 'Paz y Salvo'
